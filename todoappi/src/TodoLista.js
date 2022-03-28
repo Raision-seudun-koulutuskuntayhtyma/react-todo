ed8a1fdbc5ab95ff7b7c3314f537a54306ed3b49
Tuomas Suutari <tuomas@nepnep.net>
2022-03-28 09:41:45 +0300

    Dokumentoi TodoLista:n propsin muoto

--- todoappi/src/TodoLista.js
+++ todoappi/src/TodoLista.js
@@ -3,6 +3,26 @@ import Stack from 'react-bootstrap/Stack';
 import TodoIteemi from './TodoIteemi';
 
 function TodoLista(props) {
+    /*
+    Parametrina annettu "props" pitäisi näyttää tältä:
+
+    props = {
+        "iteemit": [
+            {
+                "id": 123,
+                "otsikko": "Joku otsikko",
+                "tehty": false
+            },
+            {
+                "id": 456,
+                "otsikko": "Joku toinen otsikko",
+                "tehty": true
+            }
+        ],
+        "merkitseTehtavaTehdyksi": <funkio>
+    }
+    */
+
     return (
         <Stack gap={2}>
             {props.iteemit.map(
